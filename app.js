32187b7880cc1000e31e86340cb6af3a1393a307
rendinel <rendinel@protonmail.com>
2021-07-25 23:45:09 +0200

    push

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const reader = new FileReader();
 const img = new Image();
 
-
-const reader = new FileReader();
-
 function uploadImage(e) {
-    reader.readAsDataURL(e.target.files[0]);
-    reader.onload = () => {
-        img.src = reader.result;
-        img.src = "bridge.jpg";
-        img.onload = () => {
-        canvas.width = img.width;
-        canvas.height = img.height;
-        ctx.drawImage(img, 0, 0);
-        const imageData = ctx.getImagedata(0,0,canvas.width,canvas.height);
-        console.log(imageData);
-        }
-    }
+  reader.onload = () => {
+    img.onload = () => {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx.drawImage(img, 0, 0);
+    };
+    img.src = reader.result;
+  };
+  reader.readAsDataURL(e.target.files[0]);
 }
 
+const imageLoader = document.getElementById("uploader");
+imageLoader.addEventListener("change", uploadImage);
 
+const change = () => {
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const data = imageData.data;
+  for (let i = 0; i < data.length; i += 4) {
+    // data[i] = 255; // red
+    // data[i+1] = 255; // green
+    data[i + 2] = 255; // blue
+  }
+  ctx.putImageData(imageData, 0, 0);
+};
 
-const imageLoader = document.getElementById('uploader');
-imageLoader.addEventListener('change',uploadImage);
+document.querySelectorAll("button")[0].addEventListener("click", change);
